9d91723b6a22331cdf4c0362b513dfa10fec98a2
Rohit Midha <rohitmidha1999@gmail.com>
2025-05-13 01:50:22 +0530

    Add full project structure: backend and frontend

---
+++ src/test/java/in/store/Billingsoftware/BillingsoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package in.store.Billingsoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingsoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
